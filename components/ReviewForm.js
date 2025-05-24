74b11009d7717b1483d59eacf023b2dfcab3fcba
José David Rodriguez Payo <josedavidrp12@gmail.com>
2025-05-24 23:04:11 +0200

    Challenge final: Añadida opción de recomendar o no producto en la review

---
+++ components/ReviewForm.js
@@ -0,0 +1,54 @@
+app.component('review-form', {
+  template:
+  /*html*/
+  `<form class="review-form" @submit.prevent="onSubmit">
+    <h3>Leave a review</h3>
+    <label for="name">Name:</label>
+    <input id="name" v-model="name">
+
+    <label for="review">Review:</label>      
+    <textarea id="review" v-model="review"></textarea>
+
+    <label for="rating">Rating:</label>
+    <select id="rating" v-model.number="rating">
+      <option>5</option>
+      <option>4</option>
+      <option>3</option>
+      <option>2</option>
+      <option>1</option>
+    </select>
+
+    
+    <input type="checkbox" id="recommend" name="Yes I would" v-model.Boolean="recommend">
+    <label for="recommend">Would you recommend this product?</label>
+    
+    <input class="button" type="submit" value="Submit">
+  </form>`,
+  data() {
+    return {
+      name: '',
+      review: '',
+      rating: null
+    }
+  },
+  methods:{
+    onSubmit(){
+        if( this.name === '' || this.review === '' || this.rating === null){
+            alert("This review is incomplete")
+            return
+        }
+        let productReview = {
+            name: this.name,
+            review: this.review,
+            rating: this.rating,
+            recommend: this.recommend ? true : false
+        }
+        console.log(productReview)
+        this.$emit('review-submitted',productReview)
+        this.name = ""
+        this.review = ""
+        this.rating = null
+        this.recommend = null
+    }
+  }
+})
